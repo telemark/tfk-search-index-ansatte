85636544e49bb5fdf1d2474f422513d3c28eedba
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-04-17 10:07:55 +0200

    Changes example settings

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@
 var config = {
   JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go',
   SEARCH_SERVICE_URL: process.env.SEARCH_SERVICE_URL || 'https://search.service.com/api',
-  SEARCH_SERVICE_INDEX: process.env.SEARCH_SERVICE_INDEX || 'www',
-  SEARCH_SERVICE_INDEX_TYPE: process.env.SEARCH_SERVICE_INDEX_TYPE || 'article',
+  SEARCH_SERVICE_INDEX: process.env.SEARCH_SERVICE_INDEX || 'employees',
+  SEARCH_SERVICE_INDEX_TYPE: process.env.SEARCH_SERVICE_INDEX_TYPE || 'employee',
   SOURCE_URL: process.env.SOURCE_URL || 'http://www.yoursite.com/api/employees.json',
   SITE_URL: process.env.SITE_URL || 'http://www.yoursite.com/employees'
 }
